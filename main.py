0b31551000bba32d6ce281a02ee68dac7a37678c
chinxi <150026069+xi1181@users.noreply.github.com>
2024-03-13 17:54:27 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-# Write you random password generator program here!
+import random
+import string
+
+pun = string.punctuation
+letters = string.ascii_letters
+num = string.digits
+combine = pun + letters + num
+def generate_password(len:int):
+    pw = ""
+    for char in range(len):
+        pw += random.choice(combine)
+    return pw
+
+password1 = generate_password(5)
+print(password1)
